fcb6c1c528b2aae27e80692ae922b59433b0dfb0
Wellington Mendes <wellingtonmm.jr@gmail.com>
2021-06-29 18:02:52 -0300

    Fixing dependencies and imports crash

--- frontend/simpleAngularSPA/src/app/app.module.ts
+++ frontend/simpleAngularSPA/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from "@angular/material/button"
+import { MatCardModule } from "@angular/material/card"
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
