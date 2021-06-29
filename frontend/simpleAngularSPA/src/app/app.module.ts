21520f07b0ea723b2fefb86928ad816bf1e0fd22
Wellington Mendes <wellingtonmm.jr@gmail.com>
2021-06-29 18:24:12 -0300

    create template components

--- frontend/simpleAngularSPA/src/app/app.module.ts
+++ frontend/simpleAngularSPA/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatButtonModule } from "@angular/material/button"
-import { MatCardModule } from "@angular/material/card"
+import { MatCardModule } from "@angular/material/card";
+import { NavComponent } from './components/templates/nav/nav.component';
+import { HeaderComponent } from './components/templates/header/header.component';
+import { FooterComponent } from './components/templates/footer/footer.component';
+import { UserAccountComponent } from './components/user/user-account/user-account.component';
+import { UserAddressComponent } from './components/user/user-address/user-address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserAccountComponent,
+    UserAddressComponent,
   ],
   imports: [
     BrowserModule,
