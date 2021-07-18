34863f8e94d77480a087663a7a99e941a688f7b4
Wellington Mendes <wellingtonmm.jr@gmail.com>
2021-07-17 22:48:26 -0300

    temporary final basePage modifications

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ScheduleComponent } from './view/schedule/schedule.component';
+import { HomeComponent } from './view/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "schedule",
+    component: ScheduleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HeaderComponent } from './components/template/header/header.component';
-import { ServicesSecctionComponent } from './components/services-secction/services-secction.component';
+import { ServicesSecctionComponent } from './components/providedServices/services-secction/services-secction.component';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatButtonModule } from '@angular/material/button'
@@ -18,7 +18,10 @@ import { HomeComponent } from './view/home/home.component';
 import { PostsComponent } from './view/posts/posts.component';
 import { ServicesComponent } from './view/services/services.component';
 import { NewsComponent } from './view/news/news.component';
-import { MyAccountComponent } from './view/my-account/my-account.component'
+import { MyAccountComponent } from './view/my-account/my-account.component';
+import { HomeHighlightsComponent } from './components/home/home-highlights/home-highlights.component';
+import { HomeColumnistComponent } from './components/home/home-columnist/home-columnist.component';
+import { ScheduleComponent } from './view/schedule/schedule.component'
 
 @NgModule({
   declarations: [
@@ -31,7 +34,10 @@ import { MyAccountComponent } from './view/my-account/my-account.component'
     PostsComponent,
     ServicesComponent,
     NewsComponent,
-    MyAccountComponent
+    MyAccountComponent,
+    HomeHighlightsComponent,
+    HomeColumnistComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
