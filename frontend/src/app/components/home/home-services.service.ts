447d9bdd601784141b12654cd1f4ba6f80abb9b1
Wellington Mendes <wellingtonmm.jr@gmail.com>
2021-09-24 23:40:42 -0300

    methods for home services created, not implemented yet, working on it

--- frontend/src/app/components/home/home-services.service.ts
+++ frontend/src/app/components/home/home-services.service.ts
@@ -1,3 +1,7 @@
+import { CarouselContent } from './../sourceComponents/home-components/home-carousel/carouselContent.model';
+import { Observable } from 'rxjs';
+import { Highlight } from './../sourceComponents/home-components/home-scroll-view/highlight.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +9,31 @@ import { Injectable } from '@angular/core';
 })
 export class HomeServicesService {
 
-  constructor() { }
+  baseURL = "http://localhost:3001/highLights"
+
+  constructor(private httpClient: HttpClient) { }
+
+  //highligths services
+  listHighLights(): Observable<Highlight[]>{
+    return this.httpClient.get<Highlight[]>(this.baseURL)
+  }
+
+  createHighLight(highlight: Highlight): Observable<Highlight>{
+    return this.httpClient.post<Highlight>(this.baseURL, highlight)
+  }
+
+  loadHighLights(id: number): Observable<Highlight>{
+    const url = `${this.baseURL}/${id}`
+    return this.httpClient.get<Highlight>(url)
+  }
+
+  //columnist services
+  listCarouselContent(): Observable<CarouselContent[]>{
+    return this.httpClient.get<CarouselContent[]>(this.baseURL)
+  }
+
+  createCarouselContent(carousel: CarouselContent): Observable<CarouselContent>{
+    return this.httpClient.post<CarouselContent>(this.baseURL, carousel)
+  }
+
 }
--- frontend/src/app/components/sourceComponents/home-components/home-scroll-view/highlight.model.ts
+++ frontend/src/app/components/sourceComponents/home-components/home-scroll-view/highlight.model.ts
@@ -1,4 +1,5 @@
 export interface Highlight {
+    
     id: number,
     orderPosition: number,
     title: string,
